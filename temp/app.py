768fb23c4f0ee81f1320d0bfa20328a7713eca3f
Qian Zhou <qzhou@stuy.edu>
2018-11-13 22:11:43 -0500

    temp update???

--- temp/app.py
+++ temp/app.py
@@ -7,15 +7,29 @@ K --
 """
 
 from flask import Flask, render_template
-from flask import request, session
-from flask import url_for, redirect, flash
+
+from flask import request, session #login function
+from flask import url_for, redirect, flash #redirect functions
+
 import os, random
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(32)
+
+
 @app.route("/")
 def root():
     return "Gallia omnis est divisa in partes tres"
+
+@app.route("/auth", methods=["POST"])
+def authentication():
+    '''Checks if the user is logged in.'''
+    usrn = request.form['username']
+    passw = request.form['password']
+    
+    return "quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appelantur. Hi omnes lingua, institutis, legibus inter se differunt. Gallos ab Aquitanis Garumna flumen, a Belgis Matrona et Sequana dividit."
+
 
 if __name__=="__main__":
     app.debug=True
